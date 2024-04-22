118fe1c9e697ec38807931eab767dab8267386c0
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2024-04-22 17:12:21 -0500

    feat: :sparkles:

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'taller3',
+  appName: 'TallerAngular',
   webDir: 'www'
 };
 
